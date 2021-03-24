c82a59e0d96e05d520bd208c1ee0ad9e34d17d7e
Valentin <valentin@girod.fr>
2021-03-24 13:56:25 +0100

    voilà2

---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  
+  }
+
+}
